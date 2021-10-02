85d977d0c9adc185b9329a1abbd2bbc19638ef39
Rohit Sharma <codedrishti@gmail.com>
2021-10-02 12:32:09 +0530

    final r files

---
+++ component/aboutCompany.js
@@ -0,0 +1,61 @@
+import Link from "next/link";
+
+function AboutCompany() {
+  return (
+    <>
+      <section id="aboutus" className="content-inner">
+        <div className="container">
+          <div className="row align-items-center">
+            <div
+              className="col-lg-6 m-b30 wow fadeInLeft"
+              data-wow-duration="2s"
+              data-wow-delay="0.2s"
+            >
+              <div className="dz-media">
+                <img src="images/about/JP-tyagi-intro-01.jpg" className="" alt="" />
+              </div>
+            </div>
+            <div
+              className="col-lg-6 m-b30 wow fadeInRight"
+              data-wow-duration="2s"
+              data-wow-delay="0.4s"
+            >
+              <div className="section-head style-1 mb-4">
+                <h6 className="sub-title bgl-primary m-b20 text-primary">
+                  About Us
+                </h6>
+                <h2 className="title">
+                  How We Can Help You Achieve Your Dream Goal
+                </h2>
+              </div>
+              <p className="text-dark">
+                We KRS Group started our opration into the real estate industry
+                in the year of 1998. And we established our self realestate
+                group in the year of 2015 with the name of KRS Home Developers.
+                We earned so much credibility & reliablity during these years.
+                KRS Group and its company KRS Home Developers is the most
+                reputed name in this industry.
+              </p>
+              <p className="m-b30 text-dark">
+                <span className="text-dark h5">We are On A Mission To Help 100,000+ People, By Providing
+                Affordable Property with World Class Aminites</span>. We Build Township
+                Whose Value Keeps on Increasing Over The Time, That why Our KRS
+                Family is Growing Too Fast. And It's Our Vision and Mission
+                Also.
+              </p>
+              {/* <img src="images/sign.png" alt="" /> */}
+              <h4 className="m-b30">KRS Home Developers Private Limited</h4>
+              <a data-formkit-toggle="1721633c3f" href="https://fabulous-inventor-6977.ck.page/1721633c3f">
+                <button className="btn btn-success d-inline-flex align-items-center move-2">
+                  <i className="fa fa-angle-right m-r10"></i>Save money and live better
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default AboutCompany;
---
+++ element/promo/gallery-images-1.js
@@ -0,0 +1,218 @@
+// import Image1 from "images/projects/grid/pic1";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import SimpleReactLightbox from 'simple-react-lightbox';
+import {SRLWrapper, useLightbox} from 'simple-react-lightbox'; 
+
+function GalleryImage() {
+  const gImage = [
+    {
+      title: "Shri Radha Rani Township - KRS Group",
+      category: ["all", "web_design", "branding"],
+      imageSrc: "/images/gallery/pic4.jpg",
+      img: (
+        <Image
+          src="/images/gallery/main-site-pic.jpg"
+          layout="responsive"
+          width={400}
+          height={370}
+        />
+      ),
+      author: "KRS Group Township",
+    },
+    {
+      title: "Shri Radha Rani Township - KRS Group",
+      category: ["all", "mobile_app", "seo"],
+      imageSrc: "/images/gallery/pic12.jpg",
+      img: (
+        <Image
+          src="/images/gallery/pic12.jpeg"
+          layout="responsive"
+          width={400}
+          height={370}
+        />
+      ),
+      author: "KRS Group Township",
+    },
+    {
+      title: "Shri Radha Rani Township - KRS Group",
+      category: ["all", "web_development", "branding"],
+      imageSrc: "/images/gallery/pic19.jpg",
+      img: (
+        <Image
+          src="/images/gallery/pic19.jpg"
+          layout="responsive"
+          width={400}
+          height={370}
+        />
+      ),
+      author: "KRS Group Township",
+    },
+    {
+      title: "Shri Radha Rani Township - KRS Group",
+      category: ["all", "web_design", "seo"],
+      imageSrc: "/images/gallery/pic4.jpg",
+      img: (
+        <Image
+          src="/images/gallery/pic3.jpg"
+          layout="responsive"
+          width={400}
+          height={370}
+        />
+      ),
+      author: "KRS Group Township",
+    },
+    {
+      title: "Shri Radha Rani Township - KRS Group",
+      category: ["all", "web_development", "branding"],
+      imageSrc: "/images/gallery/pic4.jpg",
+      img: (
+        <Image
+          src="/images/gallery/pic4.png"
+          layout="responsive"
+          width={400}
+          height={370}
+        />
+      ),
+      author: "KRS Group Township",
+    },
+    {
+      title: "Shri Radha Rani Township - KRS Group",
+      category: ["all", "web_design", "branding"],
+      imageSrc: "/images/gallery/pic4.jpg",
+      img: (
+        <Image
+          src="/images/gallery/pic5.jpg"
+          layout="responsive"
+          width={400}
+          height={370}
+        />
+      ),
+      author: "KRS Group Township",
+    },
+    {
+      title: "Shri Radha Rani Township - KRS Group",
+      category: ["all", "mobile_app", "seo"],
+      imageSrc: "/images/gallery/pic4.jpg",
+      img: (
+        <Image
+          src="/images/gallery/pic6.jpeg"
+          layout="responsive"
+          width={400}
+          height={370}
+        />
+      ),
+      author: "KRS Group Township",
+    },
+    {
+      title: "Shri Radha Rani Township - KRS Group",
+      category: ["all", "branding", "seo"],
+      imageSrc: "/images/gallery/pic4.jpg",
+      img: (
+        <Image
+          src="/images/gallery/pic7.jpeg"
+          layout="responsive"
+          width={400}
+          height={370}
+        />
+      ),
+      author: "KRS Group Township",
+    },
+    {
+      title: "Shri Radha Rani Township - KRS Group",
+      category: ["all", "mobile_app", "seo"],
+      imageSrc: "/images/gallery/pic4.jpg",
+      img: (
+        <Image
+          src="/images/gallery/pic10.png"
+          layout="responsive"
+          width={400}
+          height={370}
+        />
+      ),
+      author: "KRS Group Township",
+    },
+    {
+      title: "Shri Radha Rani Township - KRS Group",
+      category: ["all", "mobile_app", "seo"],
+      imageSrc: "/images/gallery/pic4.jpg",
+      img: (
+        <Image
+          src="/images/gallery/pic18.jpeg"
+          layout="responsive"
+          width={400}
+          height={370}
+        />
+      ),
+      author: "KRS Group Township",
+    },
+    {
+      title: "Shri Radha Rani Township - KRS Group",
+      category: ["all", "mobile_app", "seo"],
+      imageSrc: "/images/gallery/pic4.jpg",
+      img: (
+        <Image
+          src="/images/gallery/pic14.jpeg"
+          layout="responsive"
+          width={400}
+          height={370}
+        />
+      ),
+      author: "KRS Group Township",
+    },
+    {
+      title: "Shri Radha Rani Township - KRS Group",
+      category: ["all", "mobile_app", "seo"],
+      imageSrc: "/images/gallery/pic4.jpg",
+      img: (
+        <Image
+          src="/images/gallery/pic17.jpeg"
+          layout="responsive"
+          width={400}
+          height={370}
+        />
+      ),
+      author: "KRS Group Township",
+    },
+  ];
+
+  const [filter, setFilter] = useState("all");
+  const [projects, setProjects] = useState([]);
+  const { openLightbox } = useLightbox();
+
+  useEffect(() => {
+    setProjects(gImage);
+  }, []);
+  
+  return (
+        <>
+            <SimpleReactLightbox>
+                <SRLWrapper >
+                    <div className="clearfix">
+                        <ul id="masonry" className="row" data-column-width="6">
+                          {projects.map((item) =>
+                            
+                              <>
+                                <li
+                                  className="card-container col-lg-3 col-md-4 col-sm-6 web_design wow fadeInUp"
+                                  data-wow-duration="2s"
+                                  data-wow-delay="0.1s"
+                                >
+                                    <div className="dlab-box style-1 m-b30">
+                                        <div className="dlab-media">
+                                            <a href=""> {item.img}</a> 
+                                        </div>
+                                    </div>
+                                </li>
+                              </>
+                            
+                          )}
+                        </ul>
+                    </div>
+                </SRLWrapper >      
+            </SimpleReactLightbox>  
+        </>
+  );
+}
+
+export default GalleryImage;
